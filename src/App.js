3bbc47ca809fd4b9aec3cd500efc50a59cf2ecaf
임유비 <yblim@selab.co.kr>
2023-10-11 10:26:14 +0900

    빈 페이지 추가
    -잘못된 접근

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./component/Header";
 import DayList from "./component/DayList";
 import Day from "./component/Day";
 import {BrowserRouter, Route,  Routes} from "react-router-dom";
+import EmptyPage from "./component/EmptyPage";
 
 function App() {
     return (
@@ -15,6 +16,8 @@ function App() {
                 <Route path="/" exact element={<DayList />}>
                 </Route>
                 <Route path="/day/:day" element={<Day />}>
+                </Route>
+                <Route path="*" element={<EmptyPage />}>
                 </Route>
             </Routes>
         </div>
