ff469844b13bef8530053e14264744d790eab42e
임유비 <yblim@selab.co.kr>
2023-10-11 09:46:31 +0900

    단어장만들기 연습 TEST

---
+++ src/App.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import './App.css';
+
+import Header from "./component/Header";
+import DayList from "./component/DayList";
+import Day from "./component/Day";
+
+
+function App() {
+    return (
+        <div className="App">
+        <Header/>
+        <DayList />
+            <Day />
+        </div>
+
+    );
+}
+
+export default App;
---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+    return <div className="header">
+        <h1>토익단어장</h1>
+        <div className="menu">
+            <a href="#" className="link">
+                단어추가
+            </a>
+            <a href="#" className="link">
+                Day추가
+            </a>
+        </div>
+    </div>
+}
