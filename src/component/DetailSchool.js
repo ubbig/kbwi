0b151bd9fb4b86dd2cf0a4c3e60f53ffbaed0b67
임유비 <yblim@selab.co.kr>
2023-10-15 21:08:55 +0900

    학교디테일 싱크트리API 연결 및 css 추가

--- src/component/DetailSchool.js
+++ src/component/DetailSchool.js
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import dummy from "../db/data.json";
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
 import {useParams} from "react-router-dom";
 
 export default function SelectDetailSchool() {
@@ -8,18 +9,95 @@ export default function SelectDetailSchool() {
     const schoolId = Number(schoolParams.id);
     const location = Number(schoolParams.location)
     const filteredSchool = dummy.schoolList.filter(school => school.id === schoolId && school.locationCode === location);
+    const [address, setAddress] = useState('');
+    const [apt, setApt] = useState('');
+    const [detail, setDetail] = useState('');
+
+    const [randomImageUrl, setRandomImageUrl] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const jsonImageUrls = dummy.imageUrls;
+                const imageUrl = process.env.PUBLIC_URL;
+                const randomIndex = Math.floor(Math.random() * jsonImageUrls.length);
+                const selectedImageUrl = imageUrl + jsonImageUrls[randomIndex].url;
+                setRandomImageUrl(selectedImageUrl);
+
+                const responseSchool = await axios.post('/schoolList');
+                const responseSchoolList = responseSchool.data.result.response.body.dataBody.ARRAY수;
+                const randomIndexSchool = Math.floor(Math.random() * responseSchoolList.length);
+                const randomItem = responseSchoolList[randomIndexSchool];
+                setAddress(randomItem);
+
+                const responseApt = await axios.post('/saleList');
+                const responseAptList = responseApt.data.result.response.body.dataBody.ARRAY수;
+                setApt(responseAptList)
+
+                const responseAptDetail = await axios.post('/saleDetail');
+                const responseAptDetailList = responseAptDetail.data.result.response.body.dataBody.ARRAY수;
+                const AptDetailList = responseAptDetailList.slice(0, responseAptList.length);
+                setDetail(AptDetailList)
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+    }, []);
 
     return (
-        <div>
-            <Header headerText={`${filteredSchool[0]?.name}`}/>
-            <div className="locationText">
-              사진
+        <div style={{ textAlign: 'center', marginTop :'250px' }}>
+        <Header headerText={`${filteredSchool[0]?.name}`}/>
+            <div style={{height:'50px'}}></div>
+            <img
+                src={randomImageUrl}
+                alt="Random Image"
+                style={{ textAlign: 'center', width: '320px', height: '160px' }}
+            />
+            <div style={{height:'24px'}}></div>
+            <div style={{ display: 'flex', alignItems: 'center'}}>
+                <img  style={{ width: '24px', height: '24px', marginRight: '20px', marginLeft: '24px'  }}  src={require('../vendor/icon/location.png')} alt="icon"  />
+                <p style={{ fontFamily :'"Pretendard Variable", sans-serif', fontWeight: '500',fontSize:'14px', lineHeight: '3px'}}  className="category">{address.주소}</p>
             </div>
-            <div className="locationContent">
+            <div style={{ display: 'flex', alignItems: 'center'}}>
+                <img  style={{ width: '24px', height: '24px', marginRight: '20px', marginLeft: '24px'  }}  src={require('../vendor/icon/school.png')} alt="icon"  />
+                <p style={{ fontFamily :'"Pretendard Variable", sans-serif', fontWeight: '500',fontSize:'14px',lineHeight: '3px'}}  className="category">사립</p>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center'}}>
+                <img  style={{ width: '24px', height: '24px', marginRight: '20px', marginLeft: '24px'  }}  src={require('../vendor/icon/title.png')} alt="icon"  />
+                <p style={{ fontFamily :'"Pretendard Variable", sans-serif', fontWeight: '500',fontSize:'14px', lineHeight: '3px'}}  className="category">{address.학교명}</p>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center'}}>
+                <img  style={{ width: '24px', height: '24px', marginRight: '20px', marginLeft: '24px'  }}  src={require('../vendor/icon/student.png')} alt="icon"  />
+                <p style={{ fontFamily :'"Pretendard Variable", sans-serif', fontWeight: '500',fontSize:'14px',lineHeight: '3px'}}  className="category">381명</p>
+            </div>
+            <div style={{height:'20px'}}></div>
+            <div style={{textAlign:'-webkit-center'}}>
+            <button className="moreButton">더보기 ></button>
+            </div>
+            <div style={{height:'32px'}}></div>
+            <div style={{height:'5px', backgroundColor:'#DBDBDB'}}></div>
+            <div style={{height:'8px'}}></div>
+            <p style={{textAlign:'left', fontFamily :'"Pretendard Variable", sans-serif', fontWeight: 'bold',fontSize:'18px'}} >학교 주변 매물</p>
+            <div style={{height:'3px'}}></div>
+            <p style={{textAlign:'left', fontFamily :'"Pretendard Variable", sans-serif', fontWeight: 'bold',fontSize:'14px'}} >총  {apt.length}건</p>
+            <div style={{height:'1px', backgroundColor:'#DBDBDB'}}></div>
+            <div style={{height:'10px'}}></div>
+            <div style={{ maxHeight: '400px', overflow: 'scroll' }}>
+                {Array.isArray(apt) && apt.map((item, index) => (
+                    <div key={index}>
+                        <p style={{ textAlign: 'left', fontFamily: '"Pretendard Variable", sans-serif', fontWeight: '500', fontSize: '14px' }}>{item.단지명}</p>
+                        <p style={{ textAlign: 'left', fontFamily: '"Pretendard Variable", sans-serif', fontWeight: 'bold', fontSize: '20px' }}>매매 {item.매매최저일반평균가} </p>
+                        {Array.isArray(detail) && detail.length > index && (
+                            <div>
+                                <p style={{ textAlign: 'left', fontFamily: '"Pretendard Variable", sans-serif', fontWeight: '500', fontSize: '14px' }}>{item.물건종별구분명} · {detail[index].면적}㎡ / {detail[index].전용면적}㎡, 총세대수{item.총세대수} </p>
+                            </div>
+                        )}
+                        <div style={{ height: '1px', backgroundColor: '#DBDBDB' }}></div>
+                    </div>
+                ))}
+            </div>
 
-            </div>
-            <div className="locationFooter">
-            </div>
         </div>
     );
 }
